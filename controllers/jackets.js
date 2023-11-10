6e7be4226b2258219c4f886560e145827c08e36b
AkhilaMylavarapu <akhila.smr23@gmail.com>
2023-11-09 21:23:21 -0600

    implemented put

--- controllers/jackets.js
+++ controllers/jackets.js
@@ -13,33 +13,33 @@ exports.costume_list = async function (req, res) {
 
 // VIEWS
 // Handle a show all view
-exports.costume_view_all_Page = async function(req, res) {
-    try{
-    results = await Costume.find();
-    res.render('jackets', { title: 'Costume Search Results', results: results });
+exports.costume_view_all_Page = async function (req, res) {
+    try {
+        results = await Costume.find();
+        res.render('jackets', { title: 'Costume Search Results', results: results });
     }
-    catch(err){
-    res.status(500);
-    res.send(`{"error": ${err.message}}`);
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err.message}}`);
     }
-    };
-    
+};
+
 
 // for a specific Costume.
-exports.costume_detail = async function(req, res) {
+exports.costume_detail = async function (req, res) {
     console.log("detail" + req.params.id)
     try {
-    result = await Costume.findById( req.params.id)
-    res.send(result)
+        result = await Costume.findById(req.params.id)
+        res.send(result)
     } catch (error) {
-    res.status(500)
-    res.send(`{"error": document for id ${req.params.id} not found`);
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
     }
-    };
-    
+};
+
 
 // Handle Costume create on POST.
-exports.costume_create_post = async function(req, res) {
+exports.costume_create_post = async function (req, res) {
     console.log(req.body)
     let document = new Costume();
     // We are looking for a body, since POST does not have query parameters.
@@ -49,16 +49,16 @@ exports.costume_create_post = async function(req, res) {
     document.costume_type = req.body.costume_type;
     document.cost = req.body.cost;
     document.size = req.body.size;
-    try{
-    let result = await document.save();
-    res.send(result);
+    try {
+        let result = await document.save();
+        res.send(result);
     }
-    catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
     }
-    };
-    
+};
+
 
 // Handle Costume delete form on DELETE.
 exports.costume_delete = function (req, res) {
@@ -66,6 +66,22 @@ exports.costume_delete = function (req, res) {
 };
 
 // Handle Costume update form on PUT.
-exports.costume_update_put = function (req, res) {
-    res.send('NOT IMPLEMENTED: Costume update PUT' + req.params.id);
+exports.costume_update_put = async function (req, res) {
+    console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await Costume.findById(req.params.id)
+        // Do updates of properties
+        if (req.body.costume_type)
+            toUpdate.costume_type = req.body.costume_type;
+        if (req.body.cost) toUpdate.cost = req.body.cost;
+        if (req.body.size) toUpdate.size = req.body.size;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+    }
 };
