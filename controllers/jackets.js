5bf7e22cd78f5e250cd8a64c3119a5ac8d4898a0
AkhilaMylavarapu <akhila.smr23@gmail.com>
2023-11-15 10:14:15 -0600

    new jacket created successfully

--- controllers/jackets.js
+++ controllers/jackets.js
@@ -110,3 +110,18 @@ exports.costume_view_one_Page = async function (req, res) {
         res.send(`{'error': '${err}'}`);
     }
 };
+
+// Handle building the view for creating a costume.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.costume_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('jacketcreate', { title: 'Jacket Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+    
