5b94417c1c078984611702ae5d2d9c8911b07ae8
AkhilaMylavarapu <akhila.smr23@gmail.com>
2023-11-17 09:28:47 -0600

    Added schema in model account.js

--- models/account.js
+++ models/account.js
@@ -4,4 +4,17 @@
 var Account =require('./models/account');
 passport.use(new LocalStrategy(Account.authenticate()));
 passport.serializeUser(Account.serializeUser());
-passport.deserializeUser(Account.deserializeUser())
+passport.deserializeUser(Account.deserializeUser());
+
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require("passport-local-mongoose");
+const accountSchema = new Schema({
+username: String,
+password: String
+});
+accountSchema.plugin(passportLocalMongoose);
+// We export the Schema to avoid attaching the model to the
+// default mongoose connection.
+module.exports = mongoose.model("Account", accountSchema);
