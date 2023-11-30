ce2c117fcdd9e4a8e223c70c06e82cbd53ee6a79
AkhilaMylavarapu <akhila.smr23@gmail.com>
2023-11-29 18:11:17 -0600

    Done with bonus

--- models/account.js
+++ models/account.js
@@ -1,11 +1,25 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const passportLocalMongoose = require("passport-local-mongoose");
+
 const accountSchema = new Schema({
-username: String,
-password: String
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+    unique: [true, "Username must be unique"],
+    minlength: [5, "Username must be at least 5 characters long"],
+    maxlength: [20, "Username cannot be longer than 20 characters"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    minlength: [8, "Password must be at least 8 characters long"],
+    maxlength: [255, "Password cannot be longer than 255 characters"],
+  },
 });
+
 accountSchema.plugin(passportLocalMongoose);
+
 // We export the Schema to avoid attaching the model to the
 // default mongoose connection.
 module.exports = mongoose.model("Account", accountSchema);
