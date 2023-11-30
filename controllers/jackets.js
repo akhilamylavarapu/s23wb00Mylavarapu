8158e16af25798c657ee186f9c253291ee811f2a
AkhilaMylavarapu <akhila.smr23@gmail.com>
2023-11-29 18:02:04 -0600

    added buttons and secured the connections and done with validation in jacket schema

--- controllers/jackets.js
+++ controllers/jackets.js
@@ -80,7 +80,7 @@ exports.costume_update_put = async function (req, res) {
     console.log(`update on id ${req.params.id} with body
 ${JSON.stringify(req.body)}`)
     try {
-        let toUpdate = await Costume.findById(req.params.id)
+        let toUpdate = await Costume.findById(req.params.id) 
         // Do updates of properties
         if (req.body.costume_type)
             toUpdate.costume_type = req.body.costume_type;
@@ -91,8 +91,7 @@ ${JSON.stringify(req.body)}`)
         res.send(result)
     } catch (err) {
         res.status(500)
-        res.send(`{"error": ${err}: Update for id ${req.params.id}
-failed`);
+        res.send(`{"error": "${err.message}"}`);
     }
 };
 
--- models/jackets.js
+++ models/jackets.js
@@ -1,8 +1,15 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+
 const JacketSchema = mongoose.Schema({
-    costume_type: String,
-    size: String,
-    cost: Number
-})
-module.exports = mongoose.model("Jackets",JacketSchema)
+  costume_type: String,
+  size: String,
+  cost: {
+    type: Number,
+    required: true,
+    min: [10, "Cost must be at least 10"],
+    max: [200, "Cost cannot exceed 200"]
+  }
+});
+
+module.exports = mongoose.model("Jackets", JacketSchema);
 
